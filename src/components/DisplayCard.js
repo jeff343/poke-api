773113d0bc86ee06d7b7f888f398bb2325b21f90
Jeff <jeffrey.celone@gmail.com>
2023-01-13 13:11:09 -0500

    Fix mobile card styles

--- src/components/DisplayCard.js
+++ src/components/DisplayCard.js
@@ -9,15 +9,19 @@ import Grid from "@mui/material/Unstable_Grid2";
 const DisplayCard = () => {
 
     return (
-        <Card sx={{ maxWidth: 600 }}>
+        <Card sx={{ maxWidth: 600 }} component="article">
             <CardContent>
-                <Grid container spacing={2}>
-                    <Grid item md={6}>                
+                <Grid 
+                    container 
+                    spacing={{ xs: 0, sm: 2 }}
+                    p={{ xs: 0, sm: 3 }} 
+                >
+                    <Grid item xs={12} sm={6}>                
                         <Typography variant="h4" component="h3">
                             Pokemon.Name
                         </Typography>
                     </Grid>
-                    <Grid item md={6}>
+                    <Grid item sm={6}>
                         <Typography  
                             variant="h6" 
                             component="h3"
@@ -47,33 +51,39 @@ const DisplayCard = () => {
                         <Typography variant="h6" component="h4">
                             Stats:
                         </Typography>
-                        <Grid container spacing={2}>
-                            <Grid item xs={6} md={3}>
+                        <Grid 
+                            container 
+                            spacing={2}
+                            paddingTop={{ xs:0, lg: 2 }}
+                            >
+                            <Grid item xs={6} sm={3}>
                                 <Typography align="center">HP</Typography>
                                 <Typography align="center">Attack</Typography>
                                 <Typography align="center">Defense</Typography>
                             </Grid>
-                            <Grid item xs={6} md={3}>
-                                <Typography>100</Typography>
-                                <Typography>100</Typography>
-                                <Typography>100</Typography>
+                            <Grid item xs={6} sm={3}>
+                                <Typography align="center">100</Typography>
+                                <Typography align="center">100</Typography>
+                                <Typography align="center">100</Typography>
                             </Grid>
-                            <Grid item xs={6} md={3}>
+                            <Grid item xs={6} sm={3}>
                                 <Typography align="center">Sp. Att</Typography>
                                 <Typography align="center">Sp. Def</Typography>
                                 <Typography align="center">Speed</Typography>
                             </Grid>
-                            <Grid item xs={6} md={3}>
-                                <Typography>100</Typography>
-                                <Typography>100</Typography>
-                                <Typography>100</Typography>
+                            <Grid item xs={6} sm={3}>
+                                <Typography align="center">100</Typography>
+                                <Typography align="center">100</Typography>
+                                <Typography align="center">100</Typography>
                             </Grid>
                         </Grid>
                     </Grid>
                 </Grid>
             </CardContent>
             <CardActions>
-                <Button size="small">See More Info</Button>
+                <Button  
+                sx={{ mx: 'auto', width: 'auto' }}
+                size="small">See More Info</Button>
             </CardActions>
         </Card>
     )
