11480509129d8cd2bf49b618fbe3249fa23c392a
Jeff <jeffrey.celone@gmail.com>
2023-01-14 16:13:26 -0500

    Remove card component and dropdown filter

--- src/components/Searchbar.js
+++ src/components/Searchbar.js
@@ -1,58 +1,13 @@
-// import SearchIcon from '@mui/icons-material/Search';
 import Grid from '@mui/material/Unstable_Grid2';
 import FormControl from '@mui/material/FormControl';
 import FormHelperText from '@mui/material/FormHelperText';
 import TextField from '@mui/material/TextField';
-import InputLabel from '@mui/material/InputLabel';
-import Select from '@mui/material/Select';
-import MenuItem from '@mui/material/MenuItem';
 import Button from '@mui/material/Button';
 import SearchIcon from '@mui/icons-material/Search';
-import { useState } from 'react';
 
-const typesArr = [
-    "Normal",
-    "Fire",
-    "Water",
-    "Grass",
-    "Electric",
-    "Ice",
-    "Fighting",
-    "Poison",
-    "Ground",
-    "Flying",
-    "Psychic",
-    "Bug",
-    "Rock",
-    "Ghost",
-    "Dark",
-    "Dragon",
-    "Steel",
-    "Fairy"
-];
-
-const MenuProps = {
-    PaperProps: {
-        style: {
-            maxHeight: 200,
-            width: 250,
-        },
-    },
-};
 
 const Searchbar = () => {
 
-    const [types, setTypes] = useState([])
-
-    const handleChange = (event) => {
-        const {
-            target: { value },
-        } = event;
-
-        if (value.length < 3) {
-            setTypes(value);
-        };
-    };
 
     return (
         <Grid 
@@ -61,7 +16,7 @@ const Searchbar = () => {
             spacing={2}
             sx={{
                 margin: 'auto',
-                marginTop: 10,
+                marginTop: 5,
                 width: 'auto'
             }}
             maxWidth='lg'
@@ -69,8 +24,7 @@ const Searchbar = () => {
             <Grid 
                 item 
                 xs={12}
-                md={6} 
-                lg={5}
+                md={6}
                 display="flex" 
                 justifyContent="center" 
                 alignItems="center"
@@ -88,40 +42,8 @@ const Searchbar = () => {
             </Grid>
             <Grid 
                 item 
-                xs={12}
-                md={6}  
-                lg={5} 
-                display="flex" 
-                justifyContent="center" 
-                alignItems="center"
-            >
-                <FormControl component="form" variant="standard" sx={{ width: 350 }} color="secondary">
-                    <InputLabel id="pokemon-type-label">Types</InputLabel>
-                    <Select
-                        labelId="pokemon-type-label"
-                        id="pokemon-type"
-                        multiple
-                        value={types}
-                        onChange={handleChange}
-                        MenuProps={MenuProps}
-                        
-                    >
-                        {typesArr.map((type) => (
-                            <MenuItem
-                                key={type}
-                                value={type}
-                            >
-                                {type}
-                            </MenuItem>
-                        ))}
-                    </Select>
-                    <FormHelperText>Select up to 2 types</FormHelperText>
-                </FormControl>
-            </Grid>
-            <Grid 
-                item 
                 xs={12} 
-                lg={2}  
+                md={6} 
                 display="flex" 
                 justifyContent="center" 
                 alignItems="center"
@@ -130,7 +52,11 @@ const Searchbar = () => {
                     aria-label="submit-search"
                     variant="outlined" 
                     startIcon={<SearchIcon />}
-                    sx={{ height: 50, margin: 'auto' }}
+                    sx={{ 
+                        height: 50, 
+                        margin: 'auto',
+                        marginLeft: {md: 0}
+                    }}
                     color='secondary'
                 >
                     Search
--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,7 +1,5 @@
-import DisplayCard from "../components/DisplayCard";
 import Container from "@mui/material/Container";
 import Searchbar from "../components/Searchbar";
-import Grid from "@mui/material/Unstable_Grid2";
 
 
 
@@ -10,26 +8,7 @@ const HomePage = () => {
     return (
         <Container maxWidth="lg">
             <Searchbar />
-            <Grid 
-                container 
-                spacing={10} 
-                sx={{margin: 'auto'}}
-                display="flex"
-                justifyContent="center"
-            >
-                <Grid item lg={6}>
-                    <DisplayCard />
-                </Grid>
-                <Grid item lg={6}>
-                    <DisplayCard />
-                </Grid>
-                <Grid item lg={6}>
-                    <DisplayCard />
-                </Grid>
-                <Grid item lg={6}>
-                    <DisplayCard />
-                </Grid>
-            </Grid>
+            
         </Container>
     )
 };
